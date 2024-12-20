20e30271e67154ad4d435c7c457d220bef6f88a4
gubo <gubo@mmmi.sdu.dk>
2024-12-20 13:21:26 +0100

    Working on inporting data

--- examples/czii.py
+++ examples/czii.py
@@ -3,30 +3,27 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.colors import Normalize
 import zarr
+import open3d as o3d
 
 z1 = zarr.open('../data/in/czii-cryo-et-object-identification/test/static/ExperimentRuns/TS_6_4/VoxelSpacing10.000/denoised.zarr', mode='r')
-fig = plt.figure(figsize=(12, 12))
-ax = fig.add_subplot(projection='3d')
+
 scans = np.array(z1[2])
 norm = Normalize(vmin=scans.min(), vmax=scans.max())
-# ax.set_box_aspect([1,1,1])
-# ax.set_xlim3d([-0.3, 0.3])
-# ax.set_ylim3d([-0.3, 0.3])
-# ax.set_zlim3d([-0.3, 0.3])
-z1_list = np.array(z1[0]).flatten()
-print(scans.shape)
-i = 0
-print(range(scans.shape[0])[-1])
-for z in range(scans.shape[0]-1):
-    for x in range(scans.shape[1]-1):
-        if x == 0:
-            continue
-        for y in range(scans.shape[2]-1):
-            print(x, y, z)
 
-            ax.scatter(x, y, z, color=cm.viridis((scans[x][y][z])))
-            i += 1
-            break
-
-plt.show()
-print(i)
+point_cloud = o3d.geometry.PointCloud()
+points = []
+colors = []
+c_values = [np.array(cm.viridis(scans[0][0][0])[:3])]
+for z in range(scans.shape[0]):
+    for x in range(scans.shape[1]):
+        for y in range(scans.shape[2]):
+            points.append(np.array((z, x, y)))
+            color = np.array(cm.viridis(norm(scans[z][x][y]))[:3])
+            # for value in c_values:
+                # if np.linalg.norm(value - color) != 0:
+                #     c_values.append(color)
+                #     print(color)
+            colors.append(color)
+point_cloud.points = o3d.utility.Vector3dVector(points)
+point_cloud.colors = o3d.utility.Vector3dVector(colors)
+o3d.visualization.draw_geometries([point_cloud])
