e1ace5917b05b781baa7ca4de9d2873781345a79
Guglielmo Borzone <borzoneg@gmail.com>
2025-01-16 09:47:51 +0100

    Start using this as intended

--- gubokit/isaac_sim.py
+++ gubokit/isaac_sim.py
@@ -862,3 +862,72 @@ class SimRobot(Robot):
             self.manipulator_controller.apply_action(action)
         if self.lidar is not None:
             self.lidar.physisc_step()
+
+class BasicSetup(Node):
+    """This is just a basic node that opens a scene and setup the environment, copy and paste this for future projects
+    not usable as a class in other prjects
+    Use this to start isaac sim
+    # import omni
+    # from omni.isaac.kit import SimulationApp
+    # simulation_app = SimulationApp({"headless": False, "window_width": 2000, "window_height":1500})
+    """
+    def __init__(self):
+        # 
+        super().__init__("basic_setup")
+        omni.usd.get_context().open_stage(os.environ['FLUENTLY_WS_PATH'] + "/props/scene_prima_additiva_hand_e.usd")
+        self.logger = CustomLogger("Basic_setup", 
+                                   os.environ['FLUENTLY_WS_PATH'] + "/logs/basic_setup.log",
+                                   overwrite=True)
+        self.logger.info("="*10 + "Starting simulation" + "="*10)
+
+        self.setup_world()
+        
+        self.basic_ros_service_srv = self.create_service(String, 'basic_ros_service', self.basic_ros_service)
+
+        self.world.reset()
+        self.setup_task()
+    
+    # ---------------- ISAAC ---------------- #
+    def setup_world(self):
+        """
+        setup the world initializing all the needed objects
+        """
+        self.timeline = omni.timeline.get_timeline_interface()
+        self.world = World(stage_units_in_meters=1.0)
+        self.stage = omni.usd.get_context().get_stage()
+        # self.world.get_physics_context().enable_gpu_dynamics(True)
+        self.robot = SimRobot("/World/ur5e", os.environ['FLUENTLY_WS_PATH'] + "/urdf/ur5e.urdf", tcp_frame_urdf="tool0", n_gripper_articulation = 2)
+        self.robot.subscribe_to_topic('joint_states') # use a JointPublisher to send joint configuration to the robot
+        self.target_pose = self.world.scene.add(XFormPrim(prim_path="/World/target", name="target", orientation=rot_utils.euler_angles_to_quats([0, np.pi, 0])))
+        self.world.scene.add(self.robot)
+    
+    def setup_task(self):
+        pass
+
+    # ----------------- ROS ----------------- #
+    def basic_ros_service(self, request, response):
+        self.logger.info("Check dimension service requested")
+        response.ans = "success"
+        return response
+
+    # ----------- MAIN SIMULATION ----------- #
+    def run_simulation(self):
+        self.timeline.play()
+        while simulation_app.is_running():
+            rclpy.spin_once(self, timeout_sec=0.0)
+            if self.world.is_playing():
+                self.world.step(render=True)
+                self.robot.physisc_step()
+                if self.world.current_time_step_index > 100:
+                    pass
+        self.timeline.stop()
+        self.destroy_node()
+        simulation_app.close()
+
+def main():
+    rclpy.init()
+    demo = BasicSetup()
+    demo.run_simulation()
+
+if __name__ == "__main__":
+    main()
