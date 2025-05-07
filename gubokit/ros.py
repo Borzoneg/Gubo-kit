7dc33aaa847af3adfe449e951b926c88958f3627
Guglielmo Borzone <borzoneg@gmail.com>
2025-05-07 11:34:16 +0200

    Fix

--- gubokit/ros.py
+++ gubokit/ros.py
@@ -154,16 +154,17 @@ class PointCloudPublisher(Node):
         self.publisher.publish(self.msg)
 
 class JointPublisher(Node):
-    def __init__(self, topic_name: str):
+    def __init__(self, topic_name: str, joint_names=["J1", "J2", "J3", "J4", "J5", "J6"]):
         super().__init__('JointPublisher')
         self.publisher = self.create_publisher(JointState, topic_name, 10)
+        self.joint_names = joint_names
 
     def send_joint(self, q: list, names: list = None, qd: list = None, qdd: list = None):
         if len(q) != 6:
             raise ValueError
         msg = JointState()
         msg.header.stamp = self.get_clock().now().to_msg()
-        msg.name = names if names is not None else []
+        msg.name = names if names is not None else self.joint_names
         # msg.name = ['shoulder_pan_joint', 'shoulder_lift_joint', 'elbow_joint', 'wrist_1_joint', 'wrist_2_joint', 'wrist_3_joint'] # for ur5e
         msg.position = [float(x) for x in q]
         if qd is not None:
@@ -175,7 +176,7 @@ class JointPublisher(Node):
     
     def send_traj(self, qs, time_step=0.01, names: list = None, qds: list = None, qdds: list = None):
         for q in qs:
-            self.send_joint(q)
+            self.send_joint(q, names=names)
             time.sleep(time_step)
 
 class JointSubscriber(Node):
