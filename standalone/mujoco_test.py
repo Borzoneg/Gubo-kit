9109df004c10726855fbf0eeedce75ffcbfccf6d
Guglielmo Borzone <borzoneg@gmail.com>
2025-05-13 11:13:36 +0200

    update

--- standalone/mujoco_test.py
+++ standalone/mujoco_test.py
@@ -6,7 +6,6 @@ model = MjModel.from_xml_path("/home/gu/mujoco-3.3.2/model/humanoid/humanoid.xml
 data = MjData(model)
 
 with mujoco.viewer.launch_passive(model, data) as viewer:
-    input(">>>")
     while viewer.is_running():
         mujoco.mj_step(model, data)
         viewer.sync()
