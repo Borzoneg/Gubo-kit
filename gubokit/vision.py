a11b480361e8a5bdbb1ff5d72de3432d66373eb6
gubo <gubo@mmmi.sdu.dk>
2025-07-15 15:24:28 +0200

    trained cell model

--- gubokit/vision.py
+++ gubokit/vision.py
@@ -298,11 +298,10 @@ def test_YOLO_folder(yolo_model, foldername):
         frame = cv2.imread(os.path.join(foldername, f))
         results = yolo_model.predict(frame, verbose=False)
         for box in results[0].boxes:
-            label = box.cls
+            label = int(box.cls[0])
+            color =  (label*255, 0, 0)
             x_min, y_min, x_max, y_max =  map(int, box.xyxy[0].cpu().numpy())
-            x, y, w, h =  map(int, box.xywh[0].cpu().numpy())
-            cv2.rectangle(frame, (x_min, y_min), (x_max, y_max), color=(0,0,255), thickness=2)
-            cv2.circle(frame, (x, y), radius=w//2, color=(0,0,255), thickness=2)
+            cv2.rectangle(frame, (x_min, y_min), (x_max, y_max), color=color, thickness=2)
         cv2.imshow("Prediction", frame)
         ans = chr(cv2.waitKey(0) & 0xFF)
         if ans == 'q':
@@ -334,6 +333,6 @@ def collect_photos_at_pose(camera: RealSenseCamera, robot: Robot, foldername="pi
                 # cv2.waitKey(0)
 
 if __name__ == "__main__":
-    train_YOLO('/home/gu/fluently_ws/fluently_mem/data/yolo_dataset_cell/dataset.yaml')
-    # model = YOLO('/home/gu/Gubo-kit/data/out/yolo_runs/experiment1/weights/best.pt')
-    # test_YOLO_folder(model, '/home/gu/fluently_ws/fluently_mem/data/yolo_dataset_cell/images/test')
+    # train_YOLO('/home/gu/fluently_ws/fluently_mem/data/yolo_dataset_cell/dataset.yaml')
+    model = YOLO('/home/gu/Gubo-kit/data/out/yolo_runs/experiment3/weights/best.pt')
+    test_YOLO_folder(model, '/home/gu/fluently_ws/fluently_mem/data/yolo_dataset_cell/images/test')
