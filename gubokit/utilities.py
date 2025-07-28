ffa042b1984064ec073ef27514ac437a72b057af
Guglielmo Borzone <borzoneg@gmail.com>
2025-07-28 09:30:28 +0200

    adding voice module

--- gubokit/utilities.py
+++ gubokit/utilities.py
@@ -9,6 +9,12 @@ import csv
 import json
 from gubokit import ros
 import rclpy
+import whisper
+import sounddevice
+import vosk
+from collections import deque
+import queue
+
 
 class CustomLogger(logging.Logger):
     """
@@ -47,6 +53,87 @@ class CustomLogger(logging.Logger):
     
     def toggle_offon(self):
         self.console_handler.setLevel(level=logging.CRITICAL)
+
+class VoiceModule():
+    def __init__(self, commands: dict, verbose=False):
+        console_level = 'debug' if verbose else 'info'
+        self.logger = CustomLogger("Voice", "MeMVoice.log", console_level=console_level, file_level=None)
+        self.samplerate = 16000
+        self.block_size = 8000
+        self.queue = queue.Queue()
+        self.buffer_size = self.samplerate * 2
+        self.buffer = deque(maxlen=self.buffer_size)
+        vosk.SetLogLevel(-1)
+        self.model = vosk.Model("data/vosk-model-small-en-us-0.15")
+        self.vosk = vosk.KaldiRecognizer(self.model, self.samplerate)
+        self.whisper = whisper.load_model("small")
+
+        self.commands = commands
+        
+        self.stream = sounddevice.RawInputStream(
+            samplerate=self.samplerate,
+            blocksize=self.block_size,
+            dtype='int16',
+            channels=1,
+            callback=self.audio_callback
+        )
+        self.stream.start()
+
+    def audio_callback(self, indata, frames, time, status):
+        if status:
+            self.logger.warning("Audio status:", status)
+        self.queue.put(bytes(indata))
+        self.buffer.extend(np.frombuffer(indata, dtype=np.int16))
+
+    def listen_vosk(self, play_audio=False):
+        fnc_executed, text = False, ""
+        data = self.queue.get()
+        if play_audio:
+            sounddevice.play(np.frombuffer(data, dtype='int16'), samplerate=self.samplerate) # to listen back to what the module heard            
+        if self.vosk.AcceptWaveform(data):
+            result = json.loads(self.vosk.Result())
+            text = result.get("text", "").lower()
+            self.logger.debug(f"Recognized from vosk: {text}")
+            fnc_executed = self._handle_commands(text=text)
+        return fnc_executed, text
+    
+    def listen_whisper(self, play_audio=False):
+        fnc_executed, text = False, ""
+        if len(self.buffer) >= self.buffer_size:
+            audio_np = np.array(self.buffer, dtype=np.float32) / 32768.0
+            if play_audio:
+                sounddevice.play((audio_np * 32768).astype(np.int16), samplerate=self.samplerate) # to listen back top what the module heard
+                sounddevice.wait() # the play is non-blocking, the buffer is always 2 seconds (with vosk is real time) so we need this wait to listen back
+            result = self.whisper.transcribe(audio_np, fp16=False)
+            # self.logger.debug(result)
+            if len(result.get('segments')) > 0:
+                no_speech_prob = result.get('segments')[0].get("no_speech_prob", 1)
+                self.logger.debug(no_speech_prob)
+                if no_speech_prob < 0.6:
+                    text = result.get("text", "").lower()
+                    self.logger.debug(f"Recognized from whisper: {text}")
+                    fnc_executed = self._handle_commands(text=text)
+                    if fnc_executed:
+                        self.buffer.clear()
+        return fnc_executed, text
+
+    def _handle_commands(self, text):
+        for phrase, action in self.commands.items():
+            if phrase in text:
+                action()
+                return True
+        return False
+
+def use_voice_module():
+    def say_hello():
+        print("Hello")
+
+    def move_robot_home():
+        print("Move robot home")
+
+    voice_module = VoiceModule({"say hello": say_hello, "move robot home": move_robot_home}, verbose=True)
+    while True:
+        voice_module.listen_vosk(play_audio=True)
 
 def plot_joint_traj(qs: list[ndarray], title="joint_trj", hold=True):
     qs_copy = np.array(qs)
